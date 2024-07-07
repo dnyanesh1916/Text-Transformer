8e6735a93db37e1a73a733b390ba8149a94d3505
Dnyaneshwar Bukkewad <167891715+dnyanesh1916@users.noreply.github.com>
2024-07-07 20:08:40 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,9 +4,9 @@ export default function Navbar(props) {
     return (
       <>
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} py-2`}>
-    <div></div>
+    <div className="container-fluid">
     <a className="navbar-brand" href="/">Text Transformer</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
   
@@ -18,8 +18,7 @@ export default function Navbar(props) {
         <li className="nav-item">
           <Link className="nav-link" to="/about">About</Link>
         </li>
-      </ul>
-      <div className="d-inline sham ">
+    <div className="d-inline sham ">
           <button className="btn btn-primary m-1" onClick={()=>{props.toggleModem('primary')}} ></button>
           <button className="btn btn-primary m-1" onClick={()=>{props.toggleModem('secondary')}}></button>
           <button className="btn btn-success m-1" onClick={()=>{props.toggleModem('success')}} ></button>
@@ -30,6 +29,9 @@ export default function Navbar(props) {
         <input className='form-check-input' onClick={()=>{props.toggleMode(props.mode==='light'?'dark':'light')}} type='checkbox'/>
         <label className='form-check-label'>Enable darn mode</label>
    </div>
+      </ul>
+      
+    </div>
     </div>
   </nav>
   </>
